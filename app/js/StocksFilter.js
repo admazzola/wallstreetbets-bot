37ba57a2849b4be2bd0f04dc2288b67641602763
Andy <admazzola@gmail.com>
2021-01-03 21:15:36 -0500

    updated filter

---
+++ app/js/StocksFilter.js
@@ -0,0 +1,51 @@
+const checkWord = require('check-word')
+var wordChecker     = checkWord('en')
+
+const customWords = ['tendies','www','reddit','com','wallstreetbets','sr','wsb','dd','shitposting','usd','https','png','webp']
+
+module.exports = class StocksFilter {
+
+    static filterAllForStockSymbols(rawStringsArray){
+
+
+      let outputPhrases = [];
+
+      for (let str of rawStringsArray){
+
+        let filteredTokens = StocksFilter.filterPhraseForStockSymbols(str)
+
+        for(let token of filteredTokens){
+            outputPhrases.push(token)
+        }
+
+
+      }
+
+      return outputPhrases
+
+    }
+
+    static filterPhraseForStockSymbols(inputPhrase){
+
+      let inputPhraseAlphanumeric = inputPhrase.replace(/[^a-z0-9]/gmi, " ").replace(/\s+/g, " ");
+
+      let wordTokens = inputPhraseAlphanumeric.replace(/\s+/g, ' ').trim().split(' ')
+
+
+      let filteredTokens = []
+
+      for(let token of wordTokens){
+
+     
+        let isValidWord = ( wordChecker.check( token.toLowerCase() ) ||  customWords.includes(token.toLowerCase()) )
+
+        if(!isValidWord && token.length >= 2 &&   isNaN(parseInt(token))){
+          filteredTokens.push(token.toLowerCase() )
+        }
+      }
+
+      return filteredTokens
+
+    }
+
+}
