81489e3a7550f7367c72d9e97b9a830b3d312787
Andy <admazzola@gmail.com>
2021-01-03 19:05:01 -0500

    added web files

---
+++ src/config.js
@@ -0,0 +1,11 @@
+var config = {
+
+    baseurl: 'https://www.reddit.com/r/wallstreetbets.json',
+
+    testMode: true
+
+
+
+}
+
+exports.config = config
--- test/mochatest.js
+++ test/mochatest.js
@@ -1,8 +1,42 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+
+const StocksFilter = require('../src/js/StocksFilter')
+
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+
+describe('  server tests', function() {
+
+
+
+  it(" can filter test data ", async () => {
+
+
+          let testdata = ['All I know is 🚀 means buy.',
+            '',
+            'Anyone still buying NKLA?',
+            '',
+            'PLTR GANG RISE UP!!!!! This is our year 🚀🚀🚀🚀🚀🚀',
+            '',
+            'Our ARK Queen hard at work in the Ancient Times 🚀🚀🚀',
+            '',
+            'I’m ready for some 2021 tendies',
+            '',
+            'RIP Jack',
+            '',
+            'How I feel holding GME shares',
+          ]
+
+          let tokens = StocksFilter.filterAllForStockSymbols(testdata)
+
+
+
+          console.log(tokens)
+
+        assert.ok(true);
+
+      })
+
+
+  })
